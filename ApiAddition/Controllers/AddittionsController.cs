025c00c6d23ddf9afdfdbdbc7a59c060a5b26d38
Nayelydaza20 <nayelydaza3@gmail.com>
2021-10-13 10:39:03 -0400

    Agregar archivos de proyecto.

---
+++ ApiAddition/Controllers/AddittionsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAddition.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddittionsController : ControllerBase
+    {  //httpget
+        [HttpGet]
+        public int Add(int numA, int numB)
+        {
+            return numA + numB;   //funcion de suma
+        }
+    }
+}
